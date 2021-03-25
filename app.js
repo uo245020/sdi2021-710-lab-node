3bdd815db903bb946943bd1525bf3abcb27a364e
uo245020 <uo245020@uniovi.es>
2021-03-25 12:03:58 +0100

    SDI-2021-710-7.4-Recursos estáticos, peticiones POST y parámetros, Enrutamiento y comodines.”

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 let express = require('express');
 let app = express();
 
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 app.use(express.static('public'));
 // Variables
@@ -10,22 +15,21 @@ app.set('port', 8081);
 //Rutas/controladores por lógica
 require("./routes/rusuarios.js")(app); // (app, param1, param2, etc.)
 require("./routes/rcanciones.js")(app); // (app, param1, param2, etc.)
-// Variables
-app.set('port', 8081);
 app.get('/usuarios', function(req, res) {
     res.send('ver usuarios');
 })
 app.get('/canciones', function(req, res) {
     res.send('ver canciones');
 })
+
+app.get('/promo*', function (req, res) {
+    res.send('Respuesta patrón promo* ');
+})
+
 // lanzar el servidor
 app.listen(app.get('port'), function() {
     console.log("Servidor activo");
 })
-
-app.listen(8081, function(){
-    console.log("Servidor activo");
-});
 
 
 
