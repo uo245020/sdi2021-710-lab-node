8ce13a2d1b8e8f0633a42ccc5bdd4936c7fe682b
uo245020 <uo245020@uniovi.es>
2021-03-25 10:21:56 +0100

    SDI-2021-710-7.3-División de las rutas en módulos, peticiones GET y parámetros

---
+++ app.js
@@ -0,0 +1,17 @@
+// Módulos
+let express = require('express');
+let app = express();
+// Variables
+app.set('port', 8081);
+
+//Rutas/controladores por lógica
+require("./routes/rusuarios.js")(app); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app); // (app, param1, param2, etc.)
+// lanzar el servidor
+app.listen(app.get('port'), function() {
+    console.log("Servidor activo");
+})
+
+app.listen(8081, function(){
+    console.log("Servidor activo");
+});
