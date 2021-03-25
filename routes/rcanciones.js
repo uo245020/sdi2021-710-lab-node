d3e59b1e599fc19f29b018d57987947783a5cf4f
uo245020 <uo245020@uniovi.es>
2021-03-25 12:27:04 +0100

    SDI-2021-710-7.5-Vistas y motores de plantillas.”

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,12 +1,30 @@
-module.exports = function(app) {
+module.exports = function(app, swig) {
+
+
     app.get("/canciones", function (req, res) {
-        let respuesta = "";
-        if (req.query.nombre != null)
-            respuesta += 'Nombre: ' + req.query.nombre + '<br>';
-        if (typeof (req.query.autor) != "undefined")
-            respuesta += 'Autor: ' + req.query.autor;
+        var canciones = [{
+            "nombre": "Blank space",
+            "precio": "1.2"
+        }, {
+            "nombre": "See you again",
+            "precio": "1.3"
+        }, {
+            "nombre": "Uptown Funk",
+            "precio": "1.1"
+        }];
+        var respuesta = swig.renderFile('views/btienda.html', {
+            vendedor: 'Tienda de canciones',
+            canciones: canciones
+        });
         res.send(respuesta);
     });
+
+    app.get('/canciones/agregar', function (req, res) {
+        let respuesta = swig.renderFile('views/bagregar.html', {
+
+        });
+        res.send(respuesta);
+    })
 
     app.get('/suma', function (req, res) {
         let respuesta = parseInt(req.query.num1) + parseInt(req.query.num2);
