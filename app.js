04ccee267fcb991aabb16aeafdb88f31562e896b
uo245020 <uo245020@uniovi.es>
2021-04-07 23:24:55 +0200

    SDI-2021-710-8.1-Acceso a datos de Mongo desde Node.js.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 let express = require('express');
 let app = express();
 
+let mongo = require('mongodb');
 let swig = require('swig');
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -12,10 +13,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 // Variables
 app.set('port', 8081);
+app.set('db', 'mongodb://admin:sdi@tiendamusica-shard-00-00.eqonv.mongodb.net:27017,tiendamusica-shard-00-01.eqonv.mongodb.net:27017,tiendamusica-shard-00-02.eqonv.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-1126go-shard-0&authSource=admin&retryWrites=true&w=majority')
 
 //Rutas/controladores por lógica
 require("./routes/rusuarios.js")(app, swig);
-require("./routes/rcanciones.js")(app, swig);
+require("./routes/rcanciones.js")(app, swig, mongo);
 require("./routes/rautores.js")(app, swig);
 app.get('/usuarios', function(req, res) {
     res.send('ver usuarios');
