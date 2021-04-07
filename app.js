8cdc0e3bac51a15ede48ce1466dd6eb9603a33f9
uo245020 <uo245020@uniovi.es>
2021-04-07 23:30:29 +0200

    SDI-2021-710-8.2-Arquitectura para el acceso a datos.

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
 
 
 app.use(express.static('public'));
@@ -16,8 +18,8 @@ app.set('port', 8081);
 app.set('db', 'mongodb://admin:sdi@tiendamusica-shard-00-00.eqonv.mongodb.net:27017,tiendamusica-shard-00-01.eqonv.mongodb.net:27017,tiendamusica-shard-00-02.eqonv.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-1126go-shard-0&authSource=admin&retryWrites=true&w=majority')
 
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app, swig);
-require("./routes/rcanciones.js")(app, swig, mongo);
+require("./routes/rusuarios.js")(app, swig, gestorBD);
+require("./routes/rcanciones.js")(app, swig, gestorBD);
 require("./routes/rautores.js")(app, swig);
 app.get('/usuarios', function(req, res) {
     res.send('ver usuarios');
